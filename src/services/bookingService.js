c201cb7ccf0c2cc3f1898d1693962a7292976382
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-06-26 23:09:02 +0700

    update call api

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,11 +1,13 @@
-import { createAxiosBaseUrl } from "../axios";
+import { createAxiosBaseUrl, createAxiosClient } from "../axios";
 
 const axiosClient = createAxiosBaseUrl();
+
+const axiosClientToken = createAxiosClient();
 
 export const handleGetBookingInfoService = (schedule_id) => {
     return axiosClient.get(`/api/v1/booking/get-booking-info/${schedule_id}`);
 };
 
 export const createBookingScheduleService = (data) => {
-    return axiosClient.post("/api/v1/booking/booking-schedule", data);
+    return axiosClientToken.post("/api/v1/booking/booking-schedule", data);
 };
