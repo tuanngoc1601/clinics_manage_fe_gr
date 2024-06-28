1da600de7dfe8a43b860d7221197dda3b7a57dda
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-06-28 23:45:47 +0700

    check log

--- src/redux/requests/clinicRequestApi.js
+++ src/redux/requests/clinicRequestApi.js
@@ -9,6 +9,7 @@ export const getAllClinics = async (dispatch) => {
     dispatch(getAllClinicsStart());
     try {
         const res = await clinicService.handleGetAllClinicsService();
+        console.log(res);
         dispatch(getAllClinicsSuccess(res.data.data));
     } catch (err) {
         dispatch(getAllClinicsFailed());
